f1dc7a3dd7e090950746613764ac4cbf65a4e3ce
Hakan Ünlü <unluhak@outlook.com>
2021-08-02 00:45:49 +0200

    server created

--- server/index.js
+++ server/index.js
@@ -3,3 +3,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+
+// middleware
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
