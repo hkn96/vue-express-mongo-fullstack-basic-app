f9232d46d8b0f8202d682a392c57fc824d6709ce
Hakan Ünlü <unluhak@outlook.com>
2021-08-07 23:32:24 +0200

    Added Static Folder

--- server/index.js
+++ server/index.js
@@ -13,6 +13,15 @@ app.use(cors());
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
 
+//!Handle Production
+
+if (process.env.NODE_ENV === 'production') {
+  //Static Folder
+  app.use(express.static(__dirname + '/public/'));
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
